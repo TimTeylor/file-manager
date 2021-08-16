22360e834d8a8749a78160054adc6cbc8d98ea8b
Tim Teillor <timteillor@gmail.com>
2021-08-17 00:48:09 +0300

    Return all files

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ app.use(function(req, res, next) {
 })
 
 app.get('/api', function(req, res) {
-  res.json({a: '1'})
+  const path = require('path')
+  const fs = require('fs')
+  const directoryPath = path.join(__dirname, 'assets')
+  const directoryFiles = []
+
+  fs.readdir(directoryPath, function(err, files) {
+    if(err) return console.log('Unable to scan directory: ' + err)
+
+    files.forEach(function (file) {
+      directoryFiles.push(file)
+    })
+  })
+  res.json({files: directoryFiles})
 })
 
 app.listen(port, () => {
