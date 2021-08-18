9e196851f2fba0d6a2078d1fe56d5b796fb868eb
Tim Teillor <timteillor@gmail.com>
2021-08-18 22:16:06 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
+const corsOptions = {
+  origin: 'https://randevent.com'
+}
+
 const port = 5000
 
 const view = require('./services/view')
 const action = require('./services/action')
+
+app.use(cors(corsOptions))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
