a26ccf6adcc98370111db43717c887e57636a0d9
Tim Teillor <timteillor@gmail.com>
2021-08-19 02:15:50 +0300

    Fix cors

--- services/action.js
+++ services/action.js
@@ -4,8 +4,7 @@ const action = {
 
     const filedata = req.file
     if(!filedata) {
-      res.status(400)
-      res.send('Error')
+      res.status(500).send('Error')
     } else {
       res.writeHead(200, {'Connection': 'close'})
       res.end("That's all folks!")
