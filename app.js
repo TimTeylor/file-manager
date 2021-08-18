074313b77d49ba1842922da9ef0e88119aecffa9
Tim Teillor <timteillor@gmail.com>
2021-08-18 23:50:12 +0300

    Check cors

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.use(cors())
 const port = 5000
 
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header('Access-Control-Allow-Origin', '*')
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header("Access-Control-Allow-Credentials", true)
-
-  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-  // res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+  res.header("Access-Control-Allow-Credentials", true)
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+  // res.header("Access-Control-Allow-Origin", '*');
   
   next();
   // if (req.method === "OPTIONS") {
