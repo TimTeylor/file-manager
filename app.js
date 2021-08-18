a806b97e5b30324483827ea85490753eb55f84fc
Tim Teillor <timteillor@gmail.com>
2021-08-18 22:37:12 +0300

    check

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 const port = 5000
 
 const view = require('./services/view')
 const action = require('./services/action')
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS')
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.get('/api', view.getAllFiles)
 app.post('/api/upload', action.uploadFile)
