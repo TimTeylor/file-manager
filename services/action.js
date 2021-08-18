617b507a6739d113eaf0eb4b1e2ff67aabfc6846
Tim Teillor <timteillor@gmail.com>
2021-08-19 02:05:39 +0300

    Set http code

--- services/action.js
+++ services/action.js
@@ -3,8 +3,13 @@ const action = {
   uploadFile: function(req, res) {
 
     const filedata = req.file
-    if(!filedata) res.send('Error')
-    else res.send('Done')
+    if(!filedata) {
+      res.status(400)
+      res.send('Error')
+    } else {
+      res.code(200)
+      res.send('Done')
+    }
 
   }
 
