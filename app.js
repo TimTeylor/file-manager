9671ebdae42dfe37381fed2a35fb41d7457bfa16
Tim Teillor <timteillor@gmail.com>
2021-08-18 22:23:50 +0300

    Check

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ const port = 5000
 const view = require('./services/view')
 const action = require('./services/action')
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS')
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 
 app.get('/api', view.getAllFiles)
-app.put('/api/upload', action.uploadFile)
+app.post('/api/upload', action.uploadFile)
 
 app.use('/api/static', express.static('assets'))
 
