de384cb89f6929aed66a66d5bc6aeba9c336957c
Tim Teillor <timteillor@gmail.com>
2021-08-18 22:06:08 +0300

    Add res header

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const action = require('./services/action')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Content-Type", "application/json")
-  res.header("Content-Type", "form/multipart")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
 
