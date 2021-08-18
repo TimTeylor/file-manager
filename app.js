2d2383477c1d03ea0a4cd5aefd286d9a416eb2e9
Tim Teillor <timteillor@gmail.com>
2021-08-19 02:01:54 +0300

    Fix cors

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(multer({dest:'assets'}).single('filedata'))
 app.use('/api/static', express.static('assets'))
 
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next();
-})
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header("Access-Control-Allow-Headers", "*");
+//   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next();
+// })
 
 const view = require('./services/view')
 const action = require('./services/action')
