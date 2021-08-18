00216c3e6d2b60e8ee8b4116b0a9283ec848c11f
Tim Teillor <timteillor@gmail.com>
2021-08-19 02:08:51 +0300

    check cors

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const app = express()
 const port = 5000
 
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+})
+
 app.use(multer({dest:'assets'}).single('filedata'))
 app.use('/api/static', express.static('assets'))
-
-
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//   next();
-// })
 
 const view = require('./services/view')
 const action = require('./services/action')
