954efa8e9069fa4e047ae4dc3a7142324f8dff41
Tim Teillor <timteillor@gmail.com>
2021-08-18 22:22:30 +0300

    Test app use

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const app = express()
 const corsOptions = {
   origin: 'http://localhost:8080'
 }
+app.use(cors(corsOptions))
 
 const port = 5000
 
 const view = require('./services/view')
 const action = require('./services/action')
-
-app.use(cors(corsOptions))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
