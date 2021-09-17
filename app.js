6abb66533b1671b9b4af38d2c7ec6b53a71904c0
Tim Teillor <timteillor@gmail.com>
2021-09-17 04:39:02 +0200

    Delete semicolon

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "*")
   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next();
